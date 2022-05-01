deb241a6e94aa990737dc25b79f26d3c4ce86592
Snehanshu <hello@snehanshu.com>
2022-05-01 17:40:51 +0530

    dev setup

--- server.ts
+++ server.ts
@@ -1,14 +1,24 @@
 import * as express from "express";
 import { join } from "path";
 import helmet from "helmet";
+import * as proxy from "express-http-proxy";
 import backend from "./backend/src";
+
+const isProduction = process.env.NODE_ENV === "production";
 
 const app = express();
 
-app.use(helmet());
+if (isProduction) {
+  app.use(helmet());
+}
 
 app.use("/api", backend);
-app.get("*", express.static(join(__dirname, "frontend", "build")));
+app.get(
+  "*",
+  isProduction
+    ? express.static(join(__dirname, "frontend", "build"))
+    : proxy("http://localhost:2344")
+);
 
 const port = process.env.PORT || 2343;
 app.listen(port, () => console.log("Crusty is running on port", port));
