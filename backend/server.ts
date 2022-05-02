18ef916f87481dce2d325a886c5216dee7b4c095
Snehanshu <hello@snehanshu.com>
2022-05-03 02:09:24 +0530

    deployment bug fixes

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
 import { join } from 'path';
-import helmet from 'helmet';
 import proxy from 'express-http-proxy';
 import routes from './routes';
 
@@ -9,15 +8,11 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const app = express();
 
-if (isProduction) {
-  app.use(helmet());
-}
-
 app.use('/api', routes);
 app.get(
   '*',
   isProduction
-    ? express.static(join(__dirname, 'frontend', 'build'))
+    ? express.static(join(__dirname, '..', 'frontend', 'build'))
     : proxy('http://localhost:2344'),
 );
 
