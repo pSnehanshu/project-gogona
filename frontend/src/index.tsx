cb2b04ae38f31ee782436182a8726a58b96a3eba
Snehanshu <hello@snehanshu.com>
2022-05-10 14:12:25 +0530

    redirect to www subdomain

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -58,19 +58,25 @@ const theme = extendTheme({
   },
 });
 
-root.render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider theme={theme}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ChakraProvider>
-    </QueryClientProvider>
-  </React.StrictMode>,
-);
+// Redirect to www subdomain
+if (!window.location.host.startsWith('www.')) {
+  const wwwUrl = `${window.location.protocol}//www.${window.location.host}${window.location.pathname}${window.location.search}${window.location.hash}`;
+  window.location.href = wwwUrl;
+} else {
+  root.render(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider theme={theme}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ChakraProvider>
+      </QueryClientProvider>
+    </React.StrictMode>,
+  );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals();
+}
